d185de8d5baddc41415a7e9b7d4e83f0f4125c18
gaursaxe <gaursaxe>
2015-11-12 23:23:55 +0100

    Projects

---
+++ PublisherXMLTest/TestMessage/test.sql
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ts:PartyObject xmlns:ts="http://www.oracle.com/TestSchema/V0">
+  <ts:PartyDetails>
+    <ts:PartyName>Mike Ross</ts:PartyName>
+    <ts:CustomerNumber>123456789101</ts:CustomerNumber>
+    <ts:PartyAddress>Funnygade 50, 4 st. Funnyhavn S</ts:PartyAddress>
+  </ts:PartyDetails>
+  <ts:EventDetails>
+    <ts:Event>Update</ts:Event>
+  </ts:EventDetails>
+</ts:PartyObject>
---
+++ TestInsertOrMerge/TestMessage/test.sql
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ex:exampleElement xmlns:ex="http://www.example.org">
+  <ex:EventCode>242</ex:EventCode>
+  <ex:SubCode>8</ex:SubCode>
+</ex:exampleElement>
